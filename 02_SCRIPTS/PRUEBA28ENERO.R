553356b420a7caa3aeb3bb3b8346f8601bfe2381
Melmelada <87831772+Melmelada@users.noreply.github.com>
2026-01-29 16:59:42 -0600

    Initial commit

--- 02_SCRIPTS/PRUEBA28ENERO.R
+++ 02_SCRIPTS/PRUEBA28ENERO.R
@@ -1,12 +1,23 @@
 x<-rnorm(1000)
 hist(x)
 pdf("03_RESULTS/histograma_feo.pdf")
-hist(x, col="tomato")
+hist(x, col="orange")
 boxplot(x)
 dev.off()
 
 
 usethis::use_git()
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+
+usethis::use_git_config(
+  user.name = "Melmelada",
+  user.mail = "mbecerril01@alumnos.uaq.mx"
+)
+usethis::use_github()
+
 
 
 
