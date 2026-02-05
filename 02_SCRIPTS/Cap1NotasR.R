f1c6f5f9c661636ea72210f1dccb697843e77f72
Melmelada <87831772+Melmelada@users.noreply.github.com>
2026-02-05 09:33:08 -0600

    Probé abrir las librerías. Después una gráfica de puntos. Después la rueda de color y la gráfica de pie.
    Los corrí varias veces porque no los visualizaba hasta que le dí enter.

---
+++ 02_SCRIPTS/Cap1NotasR.R
@@ -0,0 +1,39 @@
+library ()
+search()
+1s(2)
+ls(2)
+ls(4)
+
+demo(graphics)
+require(datasets)
+require(grDevices); require(graphics)
+x <- stats::rnorm(50)
+opar <- par(bg = "white")
+plot(x, ann = FALSE, type = "n")
+
+abline(h = 0, col = gray(.90))
+lines(x, col = "green4", lty = "dotted")
+points(x, bg = "limegreen", pch = 21)
+
+title(main = "Simple Use of Color In a Plot",
+      xlab = "Just a Whisper of a Label",
+      col.main = "blue", col.lab = gray(.8),
+      cex.main = 1.2, cex.lab = 1.0, font.main = 4, font.lab = 3)
+par(bg = "gray")
+pie(rep(1,24), col = rainbow(24), radius = 0.9)
+title(main = "A Sample Color Wheel", cex.main = 1.4, font.main = 3)
+title(xlab = "(Use this as a test of monitor linearity)",
+      cex.lab = 0.8, font.lab = 3)
+
+pie.sales <- c(0.12, 0.3, 0.26, 0.16, 0.04, 0.12)
+names(pie.sales) <- c("Blueberry", "Cherry",
+                      "Apple", "Boston Cream", "Other", "Vanilla Cream")
+pie(pie.sales,
+    col = c("purple","violetred1","green3","cornsilk","cyan","white"))
+title(main = "January Pie Sales", cex.main = 1.8, font.main = 1)
+title(xlab = "(Don't try this at home kids)", cex.lab = 0.8, font.lab = 3)
+
+par(bg="cornsilk")
+n <- 10
+g <- gl(n, 100, n*100)
+x <- rnorm(n*100) + sqrt(as.numeric(g))
